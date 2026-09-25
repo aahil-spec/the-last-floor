7ff92694e35f033decf7e491ee64953fb0545f5c
BOSS <artworkbyfalak1@gmail.com>
2026-09-25 23:10:02 +0530

    made the elevator go to floor 4 also fixed the jolt physics error

--- scripts/key_card4.gd
+++ scripts/key_card4.gd
@@ -2,10 +2,14 @@ extends Interactable
 
 @export var prompt_text:String="Press E to pick up Security Keycard"
 @export var floor_to_unlock:int=2
+@export var room_lights:Array[Node3D]
 func interact(player_node):
 	player_node.max_unlocked_floor=floor_to_unlock
 	player_node.has_keycard=true
 	if player_node.has_method("show_message"):
 		player_node.show_message("Floor " + str(floor_to_unlock) + " Elevator Access Granted")
+		for light in room_lights:
+			if light:
+				light.visible=false
 	get_tree().call_group("washing_machines","turn_on_anomaly")
 	queue_free()
