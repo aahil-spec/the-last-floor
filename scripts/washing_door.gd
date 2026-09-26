c5658acc55ee773d47bd8cef109013bed1dc6976
BOSS <artworkbyfalak1@gmail.com>
2026-09-27 00:51:30 +0530

    fixed alot of bugs and added refined collisions+scripts

--- scripts/washing_door.gd
+++ scripts/washing_door.gd
@@ -3,10 +3,21 @@ extends Interactable
 var is_open=false
 var door_tween:Tween
 
+@export var requires_flashlight:bool=false
+@export var prompt_text: String = "Press E to open"
+
 @warning_ignore("unused_parameter")
 func interact(player_node):
+	if requires_flashlight and not is_open:
+		if player_node.has_flashlight==false:
+			prompt_text = "Cant go Inside yet."
+			if player_node.has_method("show_nessage"):
+				player_node.show_message("Cant go Inside yet.")
+			return
 	is_open=!is_open
 	
+	if is_open:
+		prompt_text=""
 	if door_tween:
 		door_tween.kill()
 	door_tween=create_tween()
